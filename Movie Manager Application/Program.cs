58c2040599e7d3a198956ee77604eefe5b8926ed
Jed Duffey <jedduffey@gmail.com>
2020-12-10 22:37:54 -0500

    move spike SQL code to separate class

--- Movie Manager Application/Program.cs
+++ Movie Manager Application/Program.cs
@@ -19,22 +19,7 @@ namespace Movie_Manager_Application
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
 
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(
-                @"Data Source=coursemaster1.csbchotp6tva.us-east-2.rds.amazonaws.com,1433;Initial Catalog=CSCI1630;User ID=rw1630;Password=Project!;"))
-                {
-                    connection.Open();
-                    // The database is closed upon Dispose() (or Close()).
-                }
-                Console.WriteLine("Successfully opened and closed the database.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Something went wrong while opening a connection to the database: { ex.Message }");
-            }
-            //Console.WriteLine("Press any key to close.");
-            //Console.ReadKey();
+            SqlHandler.foo();
         }
     }
 }
---
+++ Movie Manager Application/SqlHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Movie_Manager_Application
+{
+    class SqlHandler
+    {
+        private const string ConnectionString = @"Data Source=coursemaster1.csbchotp6tva.us-east-2.rds.amazonaws.com,1433;Initial Catalog=CSCI1630;User ID=rw1630;Password=Project!;";
+
+        public static void foo()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open(); // The database is closed upon Dispose() (or Close()).
+                }
+                Console.WriteLine("Successfully opened and closed the database.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Something went wrong while opening a connection to the database: { ex.Message }");
+            }
+        }
+    }
+}
