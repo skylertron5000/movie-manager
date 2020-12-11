7b9b9fec56e4e12c6f5d2145298a0df68eb1c94a
Jed Duffey <jedduffey@gmail.com>
2020-12-10 22:08:00 -0500

    About - rename variables and cleanup

--- Movie Manager Application/About.Designer.cs
+++ Movie Manager Application/About.Designer.cs
@@ -30,64 +30,63 @@ namespace Movie_Manager_Application
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button_close = new System.Windows.Forms.Button();
+            this.groupBox.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // groupBox
             // 
-            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.groupBox1.Location = new System.Drawing.Point(31, 25);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(731, 340);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Movie Manager Application";
+            this.groupBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox.Controls.Add(this.textBox);
+            this.groupBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox.Location = new System.Drawing.Point(31, 25);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(731, 340);
+            this.groupBox.TabIndex = 0;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Movie Manager Application";
             // 
-            // textBox1
+            // textBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(40, 47);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(641, 251);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = resources.GetString("textBox1.Text");
-            this.textBox1.ReadOnly = true;
+            this.textBox.Location = new System.Drawing.Point(40, 47);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(641, 251);
+            this.textBox.TabIndex = 0;
+            this.textBox.Text = resources.GetString("textBox1.Text");
+            this.textBox.ReadOnly = true;
             // 
-            // button_find
+            // button_close
             // 
-            this.button1.Location = new System.Drawing.Point(665, 387);
-            this.button1.Name = "button_find";
-            this.button1.Size = new System.Drawing.Size(97, 42);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Close";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button_close.Location = new System.Drawing.Point(665, 387);
+            this.button_close.Name = "button_close";
+            this.button_close.Size = new System.Drawing.Size(97, 42);
+            this.button_close.TabIndex = 1;
+            this.button_close.Text = "Close";
+            this.button_close.UseVisualStyleBackColor = true;
+            this.button_close.Click += new System.EventHandler(this.closeButton_Click);
             // 
             // About
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button_close);
+            this.Controls.Add(this.groupBox);
             this.Name = "About";
             this.Text = "About";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
             this.ResumeLayout(false);
-
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.Button button_close;
+        private System.Windows.Forms.TextBox textBox;
     }
 }
