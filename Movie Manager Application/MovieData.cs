dcf3190e5671fbcec9d4ca1ea0ec0f819bdadb83
Jed Duffey <jedduffey@gmail.com>
2020-12-12 12:00:13 -0500

    create MovieData class

---
+++ Movie Manager Application/MovieData.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Movie_Manager_Application
+{
+    class MovieData
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public string RTScore { get; set; }
+        public string BOE { get; set; }
+    }
+}
