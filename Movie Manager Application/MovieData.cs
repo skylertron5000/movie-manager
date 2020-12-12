989e9453952997ee347f722d1dc198cdc53b81a6
Jed Duffey <jedduffey@gmail.com>
2020-12-12 13:21:13 -0500

    create constructor for MovieData class

--- Movie Manager Application/MovieData.cs
+++ Movie Manager Application/MovieData.cs
@@ -3,7 +3,17 @@ namespace Movie_Manager_Application
 {
     class MovieData
     {
-        public string Title { get; set; }
+        public MovieData(string movieTitle, string year, string director, string genre, string rtScore, string boe)
+        {
+            MovieTitle = movieTitle;
+            Year = year;
+            Director = director;
+            Genre = genre;
+            RTScore = rtScore;
+            BOE = boe;
+        }
+
+        public string MovieTitle { get; set; }
         public string Year { get; set; }
         public string Director { get; set; }
         public string Genre { get; set; }
