a65724a945729d895d26f2d95b3b9d6853c7f271
Jed Duffey <jedduffey@gmail.com>
2020-12-13 17:06:29 -0500

    Add Movie button - conditional to check that Title, Year, Director, and Genre are all present

--- Movie Manager Application/AddMovie.cs
+++ Movie Manager Application/AddMovie.cs
@@ -27,25 +27,37 @@ namespace Movie_Manager_Application
 
         private void addButton_Click(object sender, EventArgs e)
         {
-            MovieData movieData = new MovieData(textBox_movieTitle.Text,
-                                                textBox_year.Text,
-                                                textBox_director.Text,
-                                                comboBox_genre.Text,
-                                                textBox_rtScore.Text,
-                                                textBox_boe.Text);
+            // If title, year, director, or genre are empty then warn the user and do not make a DB call
+            if (String.IsNullOrEmpty(textBox_movieTitle.Text.Trim()) ||
+                String.IsNullOrEmpty(textBox_year.Text.Trim()) ||
+                String.IsNullOrEmpty(textBox_director.Text.Trim()) ||
+                String.IsNullOrEmpty(comboBox_genre.Text.Trim()))
+            {
+                MessageBox.Show("You are missing one or more of the following required values:\n" +
+                                "Title, Year, Director, Genre.");
+            }
+            else
+            {
+                MovieData movieData = new MovieData(textBox_movieTitle.Text.Trim(),
+                                                    textBox_year.Text.Trim(),
+                                                    textBox_director.Text.Trim(),
+                                                    comboBox_genre.Text.Trim(),
+                                                    textBox_rtScore.Text.Trim(),
+                                                    textBox_boe.Text.Trim());
 
-            string displayText =
-                "* Add Button *\n\n" +
-                $"Movie Title: {movieData.MovieTitle}\n" +
-                $"Year: {movieData.Year}\n" +
-                $"Director: {movieData.Director}\n" +
-                $"Genre: {movieData.Genre}\n" +
-                $"RT Score: {movieData.RTScore}\n" +
-                $"BOE: {movieData.BOE}";
+                string displayText =
+                    "* Add Button *\n\n" +
+                    $"Movie Title: {movieData.MovieTitle}\n" +
+                    $"Year: {movieData.Year}\n" +
+                    $"Director: {movieData.Director}\n" +
+                    $"Genre: {movieData.Genre}\n" +
+                    $"RT Score: {movieData.RTScore}\n" +
+                    $"BOE: {movieData.BOE}";
 
-            MessageBox.Show(displayText);
+                MessageBox.Show(displayText);
 
-            SqlHandler.addMovie(movieData);
+                SqlHandler.addMovie(movieData);
+            }
         }
     }
 }
