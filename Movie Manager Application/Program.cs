e64e483dba2e70d219aaf6d6119cd211262e6c32
Jed Duffey <jedduffey@gmail.com>
2020-12-13 21:35:52 -0500

    leave schema info in Program.cs

--- Movie Manager Application/Program.cs
+++ Movie Manager Application/Program.cs
@@ -14,6 +14,16 @@ namespace Movie_Manager_Application
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+
+            //SchemaFinder.showSchema();
+            //TableCatalog TABLE_SCHEMA   TABLE_NAME COLUMN_NAME    DATA_TYPE
+            //CSCI1630       dbo Movies         Id             int
+            //CSCI1630       dbo Movies         Title varchar
+            //CSCI1630 dbo            Movies Year           int
+            //CSCI1630       dbo Movies         Director varchar
+            //CSCI1630 dbo            Movies Genre          int
+            //CSCI1630       dbo Movies         RottenTomatoesScoreint
+            //CSCI1630       dbo Movies         TotalEarned    decimal
         }
     }
 }
